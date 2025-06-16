8d3051c2ec4b14431c122bbe9bf8eb1bc8fd5805
Brewu <116376876+Brewu@users.noreply.github.com>
2025-06-16 13:54:29 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
-import Home from './pages/Home';
+import Home from './pages/BeatDetails';
 import SuccessScreen from './pages/SuccessSCreen';
 import Purchases from './pages/Purchases';
 import Homepage from './pages/HomePage';
 import ForgotPassword from './pages/ForgottenPassword';
 import { CartProvider } from './context/CartContext';
 import CartScreen from './pages/CartScreen';
-import BeatDetails from './pages/Home';
+import BeatDetails from './pages/BeatDetails';
 import MyAccountPage from './pages/MyAccountPage';
 const cors = require('cors');
 
@@ -30,11 +30,10 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/Home" element={<Home />} />
         <Route path="/Cart" element={<CartScreen />} />
         <Route path="/Account" element={<MyAccountPage />} />
         <Route path="/Success" element={<SuccessScreen />} />
-        <Route path="/beat/:id" element={<BeatDetails />} />
+        <Route path="/BeatDetails" element={<BeatDetails />} />
 
       </Routes>
     </CartProvider>
